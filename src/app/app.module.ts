c12713c987022432b76b810e6514d598261f6f93
aitnacer-nabil <palancia.gor@gmail.com>
2024-03-18 12:44:42 +0000

    Init Project fo techtitans-connect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedsComponent } from './components/feeds/feeds.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedsComponent,
+    CommentsComponent,
+    FriendsComponent,
+    ProfileComponent,
+    AuthenticationComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
