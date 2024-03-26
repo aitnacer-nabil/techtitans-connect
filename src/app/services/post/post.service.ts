9ba0f0ecfc8f2b62284bd50868acde2671059b1f
yassine jouaoudi <yass.jouaoudi@gmail.com>
2024-03-26 14:34:17 +0000

    add trash

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -8,7 +8,7 @@ import { Post } from 'src/app/models/post.model';
 })
 export class PostService {
   api_key =
-    'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZW1haWwiOiJ5YXNzaW5lQGdtYWlsLmNvbSIsInN1YiI6Inlhc3NpbmUiLCJpYXQiOjE3MTEwMzI0NzYsImV4cCI6MTcxMTAzNDI3Nn0.MxgsjTgoXhPAbCbidq8Rk8xr4Tj36cskh7xZzU3SihM';
+    'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZW1haWwiOiJ5YXNzaW5lQGdtYWlsLmNvbSIsInN1YiI6Inlhc3NpbmUiLCJpYXQiOjE3MTE0NjMwMDEsImV4cCI6MTcxMTQ2NDgwMX0.gNOpiZisFchI6-FK3Q9ndsQHcqIfU5nOXiF4s5bo4Ik';
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
     Authorization: `Bearer ${this.api_key}`,
@@ -19,9 +19,12 @@ export class PostService {
 
   create(data: Post): Observable<Post> {
     console.log('Data : ', data);
+    let fd = new FormData();
+    fd.append('body', data.body);
+    fd.append('multipartFiles', data.multipartFiles);
     return this.http.post(
       `http://localhost:8222/api/v1/post`,
-      data,
+      fd,
       this.requestOptions
     );
   }
